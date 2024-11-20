9a9d5c2c3f224f1c1744009e77707dfc7a0f8bd0
Pitla Sai Krishna <108579133+Pitlakrishna@users.noreply.github.com>
2024-11-20 23:18:41 +0530

    removed path

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(express.static(path.join(__dirname, "./client/build")))
+// app.use(express.static(path.join(__dirname, "./client/build")))
 
 app.use('/api/v1/task', taskRoute)
 app.use('/api/v1/auth', authRoutes)
 
-app.use('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+// app.use('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, './client/build/index.html'))
+// })
 
 const PORT = process.env.PORT || 8080;
 
